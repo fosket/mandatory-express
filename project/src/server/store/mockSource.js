4b00942613404e70d851e1698eaff0ac63f00991
Johanmansson <johan.mansson@hotmail.se>
2018-04-17 07:17:56 +0200

    final changes

--- project/src/server/store/mockSource.js
+++ project/src/server/store/mockSource.js
@@ -15,20 +15,14 @@ const posts = [
     {
         "userId": 1,
         "id": 1,
-        "title": "Julius Caesar",
-        "body": "Veni vidi vici"
+        "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
     },
     {
         "userId": 1,
         "id": 2,
-        "title": "Julius Caesar",
-        "body": "Alea iacta est"
-    },
-    {
-        "userId": 1,
-        "id": 3,
-        "title": "Julius Caesar",
-        "body": "Beati Hispani quibus bibere vivere est"
+        "title": "qui est esse",
+        "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
     }
 ]
 
